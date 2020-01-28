1edc0a79cab137d75187035eeb99c4bcd72627bc
Eddie Brazier (RIT Student) <ecb9413@ad.rit.edu>
2020-01-27 21:57:29 -0500

    stuff changed?

---
+++ FishingHorror/Assets/Scripts/Interactables.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Interactable Objects", menuName = "ScriptableObjects/Interactables", order =1)]
+public class Interactables : ScriptableObject
+{
+    public List<GameObject> interactableList;
+}
