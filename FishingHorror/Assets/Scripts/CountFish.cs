b883f62cfb1789d523a721e011e5865cb2c2bf1d
Eddie Brazier (RIT Student) <ecb9413@ad.rit.edu>
2020-02-12 13:27:51 -0500

    working on sequencing

--- FishingHorror/Assets/Scripts/CountFish.cs
+++ FishingHorror/Assets/Scripts/CountFish.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public enum CountMode
 {
@@ -10,24 +11,51 @@ public enum CountMode
 
 public class CountFish : MonoBehaviour
 {
+    //field for static instance of this object
+    public static CountFish instance;
+
     //field for the number of fish the player has caught
     public int fishCount = 0;
+
+    //list of caught fish
+    private List<GameObject> caughtFish;
 
     //field for the current counting mode being used by the game
     private CountMode countMode = CountMode.Bucket;
 
     //TODO: Incorporate Fog counting mode into this script
 
+    //reference to gameobject that holds the fog
+    [SerializeField]
+    private GameObject fogObject;
+
+    //field for the volume component on the fog object
+    private Volume fog;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //initialize static instance
+        if(instance != null)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        //intialize fish list
+        caughtFish = new List<GameObject>();
+
+        //initialize fog
+        fog = fogObject.GetComponent<Volume>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //TODO: check fish count to determine whether or not to start next stage of the game
+        
     }
 
     /// <summary>
@@ -36,9 +64,9 @@ public class CountFish : MonoBehaviour
     /// <param name="other"> the gameobject that entered the trigger </param>
     private void OnTriggerEnter(Collider other)
     {
-        //check if the object that just entered the bucket was a fish
+        //check if the object that just entered the bucket was a fish that hasn't been caught
         //TODO: replace tag usage once fish are being tracked in a script
-        if(other.gameObject.tag == "Fish")
+        if(other.gameObject.tag == "Fish" && !caughtFish.Contains(other.gameObject))
         {
             //increase fish count
             fishCount++;
@@ -47,6 +75,7 @@ public class CountFish : MonoBehaviour
             Debug.Log("Fish dropped into bucket. \nFish Count: " + fishCount);
 
             //TODO: add fish to a list of caught fish so it won't count more than once as a new fish 
+            caughtFish.Add(other.gameObject);
         }
     }
 }
---
+++ FishingHorror/Assets/Scripts/SequenceManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceManager : MonoBehaviour
+{
+    //field for the next threshold of fish the player has to catch to advance to the next sequence
+    private int nextThreshold = 0;
+
+    //list of fish thresholds based on events the player experiences
+    public List<int> fishThresholdList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: check fish count to determine whether or not to start next stage of the game
+        if(CountFish.instance.fishCount == nextThreshold)
+        {
+            //TODO: allow time to advance to next stage
+            
+
+            //increase fish threshold
+            nextThreshold += fishThresholdList[0];
+
+            //remove first member of the fish threshold list
+            fishThresholdList.RemoveAt(0);
+        }
+    }
+}
