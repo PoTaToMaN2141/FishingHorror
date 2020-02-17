d0b7c5ab5fddfcce31cf73e1db4ed0c6923b4ab6
Eddie Brazier (RIT Student) <ecb9413@ad.rit.edu>
2020-02-17 15:52:10 -0500

    working on game sequencing

--- FishingHorror/Assets/Scripts/SequenceManager.cs
+++ FishingHorror/Assets/Scripts/SequenceManager.cs
@@ -10,10 +10,20 @@ public class SequenceManager : MonoBehaviour
     //list of fish thresholds based on events the player experiences
     public List<int> fishThresholdList;
 
+    //list of time thresholds for game stages
+    [Tooltip("Note: When setting time thresholds, try to keep increments in hours! There are 3600 seconds in an hour.")]
+    public List<int> timeThresholdList;
+
+    //field for the object that holds the daynight script and the script itself
+    [SerializeField]
+    private GameObject timeObject;
+    private DayNight timeCycle;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //save reference to time cycle
+        timeCycle = timeObject.GetComponent<DayNight>();
     }
 
     // Update is called once per frame
@@ -23,13 +33,28 @@ public class SequenceManager : MonoBehaviour
         if(CountFish.instance.fishCount == nextThreshold)
         {
             //TODO: allow time to advance to next stage
-            
+            //TODO: If time hasn't completely passed from the previous stage, quickly advance it forward
+            DayNight.Paused = false;
 
             //increase fish threshold
-            //nextThreshold += fishThresholdList[0];
+            nextThreshold += fishThresholdList[0];
+            Debug.Log("Next Threshold for Fish: " + nextThreshold + "\nFish Count: " + CountFish.instance.fishCount);
 
             //remove first member of the fish threshold list
-            //fishThresholdList.RemoveAt(0);
+            fishThresholdList.RemoveAt(0);
+
+            //TODO: change the kinds of fish the player can catch
+        }
+
+        //check if the current time has reached the stopping point for the stage
+        if(timeCycle.CurrentTime >= timeThresholdList[0])
+        {
+            //Debug for stopped time
+            Debug.Log("Time has stopped. Catch more fish to resume time.");
+
+            //pause time and set new time threshold
+            DayNight.Paused = true;
+            timeThresholdList.RemoveAt(0);
         }
     }
 }
